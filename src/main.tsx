af0c33bfb5b0e80a42baa01d887f3f76eb4d9d64
proyas akhanda <proyasakhanda2005@gmail.com>
2026-07-18 12:16:31 +0600

    fixed some errors in main file and tscofigjson

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// @ts-ignore: side-effect import without type declarations
 import 'bootstrap/dist/css/bootstrap.min.css';
+// @ts-ignore: side-effect import without type declarations
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
